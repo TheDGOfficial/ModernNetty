40fdcc510fdb03e7ef6e385c01935fcd203f8484
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-09-17 00:58:43 +0300

    Bump Loom

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.nio.charset.StandardCharsets
 
 plugins {
   id("java")
-  id("net.fabricmc.fabric-loom") version ("1.18.1")
+  id("net.fabricmc.fabric-loom") version ("1.18.2")
 }
 
 group = "pet.liawr"
